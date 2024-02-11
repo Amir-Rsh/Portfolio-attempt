a2c93d13499fe59f7c95005ae9c8fa326fdc2a82
Amir-Rsh <amirrashidinia93@gmail.com>
2024-02-11 23:42:43 +0000

    worked on webkit compatibility

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -5,19 +5,6 @@ export default function IntroPage() {
         <div className="bubbles">
           <span style={{ "--i": "24" }}></span>
           <span style={{ "--i": "12" }}></span>
-          {/* <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "27" }}></span>
-          <span style={{ "--i": "9" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "29" }}></span>
-          <span style={{ "--i": "8" }}></span>
-          <span style={{ "--i": "13" }}></span>
-          <span style={{ "--i": "19" }}></span>
-          <span style={{ "--i": "11" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "10" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "12" }}></span>
           <span style={{ "--i": "18" }}></span>
           <span style={{ "--i": "27" }}></span>
           <span style={{ "--i": "9" }}></span>
@@ -54,7 +41,20 @@ export default function IntroPage() {
           <span style={{ "--i": "19" }}></span>
           <span style={{ "--i": "11" }}></span>
           <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "10" }}></span> */}
+          <span style={{ "--i": "10" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "12" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "27" }}></span>
+          <span style={{ "--i": "9" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "29" }}></span>
+          <span style={{ "--i": "8" }}></span>
+          <span style={{ "--i": "13" }}></span>
+          <span style={{ "--i": "19" }}></span>
+          <span style={{ "--i": "11" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "10" }}></span>
         </div>
       </div>
       <div
