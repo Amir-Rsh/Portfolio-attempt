3fd0e47f97de97d970d4170e6e0c1f05ac39b570
Amir-Rsh <amirrashidinia93@gmail.com>
2024-02-11 19:46:27 +0000

    initial commite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import IntroPage from "./components/IntroPage";
+
+function App() {
+  return (
+    <>
+      <IntroPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/IntroPage.jsx
@@ -0,0 +1,97 @@
+export default function IntroPage() {
+  return (
+    <>
+      <div className="container">
+        <div className="bubbles">
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "12" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "27" }}></span>
+          <span style={{ "--i": "9" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "29" }}></span>
+          <span style={{ "--i": "8" }}></span>
+          <span style={{ "--i": "13" }}></span>
+          <span style={{ "--i": "19" }}></span>
+          <span style={{ "--i": "11" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "10" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "12" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "27" }}></span>
+          <span style={{ "--i": "9" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "29" }}></span>
+          <span style={{ "--i": "8" }}></span>
+          <span style={{ "--i": "13" }}></span>
+          <span style={{ "--i": "19" }}></span>
+          <span style={{ "--i": "11" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "10" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "12" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "27" }}></span>
+          <span style={{ "--i": "9" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "29" }}></span>
+          <span style={{ "--i": "8" }}></span>
+          <span style={{ "--i": "13" }}></span>
+          <span style={{ "--i": "19" }}></span>
+          <span style={{ "--i": "11" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "10" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "12" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "27" }}></span>
+          <span style={{ "--i": "9" }}></span>
+          <span style={{ "--i": "18" }}></span>
+          <span style={{ "--i": "29" }}></span>
+          <span style={{ "--i": "8" }}></span>
+          <span style={{ "--i": "13" }}></span>
+          <span style={{ "--i": "19" }}></span>
+          <span style={{ "--i": "11" }}></span>
+          <span style={{ "--i": "24" }}></span>
+          <span style={{ "--i": "10" }}></span>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "100%",
+          position: "fixed",
+          top: "0",
+          right: "0",
+          bottom: "0",
+          left: "0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "darkseagreen",
+          marginLeft: "6%",
+          marginRight: "6%",
+          flexDirection: "column",
+          textAlign: "center",
+        }}
+      >
+        <h1>HELLO WORLD!</h1>
+        <h2>
+          I'M AMIR RASHIDINIA, THANKS FOR CHECKING OUT MY PORTFOLIO WEBSITE
+        </h2>
+        <h3>
+          I'm a junior Software Developer interested in full stack well
+          development
+        </h3>
+        <ul>
+          <li>
+            <a href="/projects">→ see my projects</a>
+          </li>
+          <li>
+            <a href="/about">→ about me</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
