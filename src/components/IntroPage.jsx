b67e9b4fc009f9786ebca47f5e6c26385a5f43fa
Amir-Rsh <amirrashidinia93@gmail.com>
2024-03-19 07:36:28 +0000

    made the new landing page

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -1,100 +1,48 @@
 export default function IntroPage() {
   return (
     <>
-      <div className="container">
-        <div className="bubbles">
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "12" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "27" }}></span>
-          <span style={{ "--i": "9" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "29" }}></span>
-          <span style={{ "--i": "8" }}></span>
-          <span style={{ "--i": "13" }}></span>
-          <span style={{ "--i": "19" }}></span>
-          <span style={{ "--i": "11" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "10" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "12" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "27" }}></span>
-          <span style={{ "--i": "9" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "29" }}></span>
-          <span style={{ "--i": "8" }}></span>
-          <span style={{ "--i": "13" }}></span>
-          <span style={{ "--i": "19" }}></span>
-          <span style={{ "--i": "11" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "10" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "12" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "27" }}></span>
-          <span style={{ "--i": "9" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "29" }}></span>
-          <span style={{ "--i": "8" }}></span>
-          <span style={{ "--i": "13" }}></span>
-          <span style={{ "--i": "19" }}></span>
-          <span style={{ "--i": "11" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "10" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "12" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "27" }}></span>
-          <span style={{ "--i": "9" }}></span>
-          <span style={{ "--i": "18" }}></span>
-          <span style={{ "--i": "29" }}></span>
-          <span style={{ "--i": "8" }}></span>
-          <span style={{ "--i": "13" }}></span>
-          <span style={{ "--i": "19" }}></span>
-          <span style={{ "--i": "11" }}></span>
-          <span style={{ "--i": "24" }}></span>
-          <span style={{ "--i": "10" }}></span>
-        </div>
+      <div class="container">
+        <p id="line1">
+          <span id="inputSign1">{">"}</span>HELLO WORLD!
+        </p>
       </div>
-      <div
-        className="introText"
-        style={{
-          height: "100%",
-          position: "fixed",
-          top: "0",
-          right: "0",
-          bottom: "0",
-          left: "0",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "darkseagreen",
-          marginLeft: "6%",
-          marginRight: "6%",
-          flexDirection: "column",
-          textAlign: "center",
-        }}
-      >
-        <h1>HELLO WORLD!</h1>
-        <h2>
-          I'M AMIR RASHIDINIA, THANKS FOR CHECKING OUT MY PORTFOLIO WEBSITE
-        </h2>
-        <h3>
-          Junior Software Developer interested in full stack web development
-        </h3>
-        <div
-          style={{
-            backgroundColor: "rgba(128, 0, 128, 70%)",
-            borderRadius: "5%",
-            color: "black",
-            padding: "1%",
-          }}
-        >
-          <a href="/projects">→my projects </a>
-          <a href="/about"> →about me </a>
-        </div>
+      <br />
+      <div class="container">
+        <p id="line2">
+          <span id="inputSign2">{">"}</span>I AM AMIR RASHIDINIA
+        </p>
       </div>
+      <br />
+      <div class="container">
+        <p id="line3">
+          <span id="inputSign3">{">"}</span>WELCOME TO MY PORTFOLIO
+        </p>
+      </div>
+      <br />
+      <div id="container">
+        <p id="slider">{">"}</p>
+        <button id="enter">ENTER WEBSITE</button>
+      </div>
+      <p style="margin-bottom: 0%">
+        <img
+          class="logos"
+          src="https://i.ibb.co/XZwQdtV/Linked-In-logo-initials.png"
+          width="150px"
+          alt=""
+        />
+        <img
+          id="profilePic"
+          src="https://i.ibb.co/nn6bFZ6/149586453.png"
+          alt=""
+        />
+        <img
+          class="logos"
+          src="https://i.ibb.co/zhzDFnB/git-2.png"
+          width="150px"
+          height="150px"
+          alt=""
+        />
+      </p>
     </>
   );
 }
