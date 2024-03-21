ef7a8e1919becc9052b9ccecc7b3514d09ce4248
Amir-Rsh <amirrashidinia93@gmail.com>
2024-03-21 06:03:26 +0000

    worked on responsiveness

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,22 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import LoadingPage from "./LoadingPage";
 
 export default function HomePage() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3900);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return isLoading ? (
+    <LoadingPage />
+  ) : (
     <>
       <nav>
         <Link to="https://portfolio-amir-rashidinia.netlify.app/home">
-          {" "}
-          <img
-            src="https://www.freepnglogos.com/uploads/logo-home-png/home-logo-images-black-5.png"
-            width="40px"
-            alt=""
-          />
+          <img src="https://i.ibb.co/bR7scSZ/logoooo.png" width="40px" alt="" />
         </Link>
         <Link to="https://www.linkedin.com/in/amir-rashidinia">
           <img
             src="https://cdn.icon-icons.com/icons2/2428/PNG/512/linkedin_black_logo_icon_147114.png"
             width="40px"
-            style={{ margin: "0 30px 0 30px" }}
+            style={{ margin: "0 50px 0 50px" }}
             alt=""
           />
         </Link>
@@ -28,16 +35,15 @@ export default function HomePage() {
           />
         </Link>
       </nav>
-      <div id="2ndProject" className="container1">
+      <div className="container1">
         <div className="trapezoid1">
           <p id="aboutMeText">
             {">"}I am a Junior Software Developer with a passion for turning
             code into cool looking websites and apps. I recently graduated from
-            a coding bootcamp and i can't wait to start my journey in the world
-            of tech. I constantly work on new projects to increase my knowledge
-            of coding and add to my tech stack.
+            a coding bootcamp and can't wait to start my journey in the world of
+            tech. I constantly work on new projects to increase my knowledge of
+            coding and add to my tech stack.
           </p>
-          {/* <p id="bisect2"></p> */}
         </div>
         <div id="bisect">
           <p id="about">ABOUT ME</p>
@@ -116,44 +122,69 @@ export default function HomePage() {
                 flexDirection: "column",
               }}
             >
-              <button
-                id="tipperButtonTop"
-                style={{
-                  height: "100px",
-                  width: "100px",
-                  backgroundColor: "black",
-                  borderRadius: "5%",
-                  borderBlockColor: "black",
-                  borderStyle: "solid",
-                  borderWidth: "0px",
-                  color: "green",
-                  fontWeight: "bold",
-                  fontSize: "1rem",
-                }}
+              <Link
+                to="https://front-end-eight-eta-57.vercel.app/"
+                target="_blank"
               >
-                Project Page
-              </button>
-              <button
-                id="tipperButtonBottom"
-                style={{
-                  height: "100px",
-                  width: "100px",
-                  marginTop: "20px",
-                  backgroundColor: "black",
-                  borderRadius: "5%",
-                  borderBlockColor: "black",
-                  borderStyle: "solid",
-                  borderWidth: "0px",
-                  color: "green",
-                  fontWeight: "bold",
-                  fontSize: "1rem",
-                }}
+                <button
+                  id="tipperButtonTop"
+                  style={{
+                    height: "100px",
+                    width: "100px",
+                    backgroundColor: "black",
+                    borderRadius: "5%",
+                    borderBlockColor: "black",
+                    borderStyle: "solid",
+                    borderWidth: "0px",
+                    color: "green",
+                    fontWeight: "bold",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Project Page
+                </button>
+              </Link>
+              <Link
+                to="https://github.com/Tipper-final-project"
+                target="_blank"
               >
-                Code on GitHub
-              </button>
+                <button
+                  id="tipperButtonBottom"
+                  style={{
+                    height: "100px",
+                    width: "100px",
+                    marginTop: "20px",
+                    backgroundColor: "black",
+                    borderRadius: "5%",
+                    borderBlockColor: "black",
+                    borderStyle: "solid",
+                    borderWidth: "0px",
+                    color: "green",
+                    fontWeight: "bold",
+                    fontSize: "1rem",
+                  }}
+                >
+                  Code on GitHub
+                </button>
+              </Link>
             </div>
           </div>
         </div>
+      </div>
+      <div className="container3">
+        <div className="trapezoid5">
+          <p id="aboutMeText">
+            {">"}I am a Junior Software Developer with a passion for turning
+            code into cool looking websites and apps. I recently graduated from
+            a coding bootcamp and can't wait to start my journey in the world of
+            tech. I constantly work on new projects to increase my knowledge of
+            coding and add to my tech stack.
+          </p>
+        </div>
+        <div id="bisect">
+          <p id="about">ABOUT ME</p>
+        </div>
+        <div className="trapezoid6"></div>
       </div>
     </>
   );
---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,10 @@
+export default function LoadingPage() {
+  return (
+    <>
+      <div className="loadingPage">
+        <div id="dotLoading"></div>
+        <p style={{ transform: "translatey(-160px)" }}>Loading</p>
+      </div>
+    </>
+  );
+}
